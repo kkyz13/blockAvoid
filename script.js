6ca4ce1ffb8b72262470388a0d1f35c8413c2ab4
Kenneth Koh <kkyz13@gmail.com>
2024-02-21 15:03:44 +0800

    HTML, CSS and script update
    Playable State

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+const fallArray = document.querySelectorAll(".fallbox"); //this is to grab all fallbox inside HTML
+const laneArray = document.querySelectorAll(".lane"); // grabs the lanes as an array
+const playAreaArray = document.querySelectorAll(".playarea"); //grabs the playAreaArray bottom selector as an array
+let activeLaneIdx = 0;
+let score = 0;
+let fallSpeed = 1;
+
 function delay(milliseconds) {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
   });
 }
 
-const fallArray = document.querySelectorAll(".fallbox"); //this is to grab all fallbox inside HTML
 fallArray.forEach((boxEntry) => {
+  score = 1;
   //this adds animationend to all the .fallbox
   boxEntry.addEventListener("animationend", () => {
     boxEntry.classList.remove("fallingAnimation");
+    score++;
   });
 });
-
-const laneArray = document.querySelectorAll(".lane"); // grabs the lanes as an array
-const playAreaArray = document.querySelectorAll(".playarea"); //grabs the playAreaArray bottom selector as an array
-let activeLaneIdx = 0;
 
 //CONTROLLER//
 window.addEventListener("keydown", moveMe);
@@ -25,10 +29,10 @@ function moveMe(e) {
       activeLaneIdx = 0;
     } else {
       playAreaArray.forEach((laneEntry) => {
-        laneEntry.classList.remove("active");
+        laneEntry.classList.remove("playactive");
       });
       activeLaneIdx--;
-      playAreaArray[activeLaneIdx].classList.add("active");
+      playAreaArray[activeLaneIdx].classList.add("playactive");
       laneActiver();
     }
   } else if (e.key === "ArrowRight") {
@@ -36,10 +40,10 @@ function moveMe(e) {
       activeLaneIdx = 2;
     } else {
       playAreaArray.forEach((laneEntry) => {
-        laneEntry.classList.remove("active");
+        laneEntry.classList.remove("playactive");
       });
       activeLaneIdx++;
-      playAreaArray[activeLaneIdx].classList.add("active");
+      playAreaArray[activeLaneIdx].classList.add("playactive");
       laneActiver();
     }
     return;
@@ -53,22 +57,27 @@ function laneActive() {
   laneArray[activeLaneIdx].classList.add("laneactive");
 }
 function laneActiver() {
-  //adds an "eventlistener" activer for aiding collision detection
+  //adds an "eventlistener" boxactive for aiding collision detection
   laneActive();
   fallArray.forEach((laneEntry) => {
-    laneEntry.classList.remove("activer");
+    laneEntry.classList.remove("boxactive");
   });
   for (let j = activeLaneIdx; j <= fallArray.length - 1; ) {
-    fallArray[j].classList.add("activer");
+    fallArray[j].classList.add("boxactive");
     j += 3;
   }
 }
 
-laneActiver();
 function randomLane() {
   return Math.ceil(Math.random() * 9);
 }
 
+function speedUp() {
+  for (boxEntry of fallArray) {
+    boxEntry.style.animationDuration = `${fallSpeed}s`;
+  }
+  fallSpeed -= 0.05;
+}
 function dropBoxOnly() {
   fallArray[randomLane() - 1].classList.add("fallingAnimation");
   //   fallArray[randomLane() - 1].classList.add("fallingAnimation");
@@ -82,12 +91,16 @@ function dropBoxOnly() {
 // }, 1000);
 
 function gameStart() {
+  score = 0;
+  laneActiver();
   startBtn.removeEventListener("click", gameStart);
   if ((clearLaneCollision = document.querySelector(".collisioned"))) {
     clearLaneCollision.classList.remove("collision", "collisioned");
   }
   delay(1000);
   setInterval(collisionDetect, 1);
+  setInterval(scoreCounter, 200);
+  startSpeed = setInterval(speedUp, 2000);
   startDropbox = setInterval(dropBoxOnly, 200);
 }
 
@@ -97,12 +110,14 @@ startBtn.addEventListener("click", gameStart);
 function gameLose() {
   startBtn.addEventListener("click", gameStart);
   fallArray.forEach((laneEntry) => {
-    laneEntry.classList.remove("activer");
-    laneEntry.classList.remove("fallingAnimation");
+    laneEntry.classList.remove("fallingAnimation", "boxactive");
+    laneEntry.removeAttribute("style");
+    // laneEntry.classList.remove("fallingAnimation");
   });
   clearInterval(startDropbox);
+  clearInterval(startSpeed);
+  fallSpeed = 1;
   console.log("You lose");
-  gameRunning = false;
 }
 
 const stopBtn = document.querySelector("#stop");
@@ -118,7 +133,11 @@ function collisionDetect() {
 }
 function boxCollisionDetect(droppingBox) {
   let box = droppingBox.getBoundingClientRect();
-  if (box.y > 555 && box.y < 570 && droppingBox.classList.contains("activer")) {
+  if (
+    box.y > 555 &&
+    box.y < 582 &&
+    droppingBox.classList.contains("boxactive")
+  ) {
     console.log("COLLISION");
     gameLose();
     laneArray[activeLaneIdx].classList.add("collision");
@@ -130,6 +149,10 @@ function boxCollisionDetect(droppingBox) {
   }
 }
 
+function scoreCounter() {
+  scoreWriter = document.querySelector("#score");
+  scoreWriter.innerText = score;
+}
 //DEBUGGER ------------
 // const box1 = document.querySelector(".p3");
 // let rect = box1.getBoundingClientRect();
